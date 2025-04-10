f8e512f72ed6ff0bea71a24a410e2147c949e079
JIEUNI <jieundaeun@naver.com>
2025-04-10 16:08:21 +0900

    [feat] 제목 30자 초과하는 경우 예외 처리

--- src/main/java/org/sopt/domain/post/validator/PostValidator.java
+++ src/main/java/org/sopt/domain/post/validator/PostValidator.java
@@ -3,9 +3,15 @@ package org.sopt.domain.post.validator;
 import org.sopt.domain.post.exception.InvalidTitleException;
 
 public class PostValidator {
+    private static final int MAX_TITLE_LENGTH = 30;
+
     public static void validateTitle(String title) {
         if (title == null || title.trim().isEmpty()) {
             throw new InvalidTitleException("제목은 공백이거나 비어 있을 수 없습니다.");
+        }
+
+        if (title.length() > MAX_TITLE_LENGTH) {
+            throw new InvalidTitleException("제목은 30자 이내여야 합니다.");
         }
     }
 }
