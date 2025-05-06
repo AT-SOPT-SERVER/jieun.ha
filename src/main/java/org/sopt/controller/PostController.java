602b2e7bc07219a3fe4ee017dff4d2ef4684e993
JIEUNI <jieundaeun@naver.com>
2025-05-06 16:58:43 +0900

    [mod] #6 RequestMapping 사용해 중복된 URI 관리

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -4,12 +4,12 @@ import org.sopt.domain.post.Post;
 import org.sopt.dto.base.BaseResponse;
 import org.sopt.dto.request.PostRequest;
 import org.sopt.service.PostService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/board")
 // 요청을 받아 처리하는 역할
 public class PostController {
     private final PostService postService;
@@ -18,27 +18,27 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PostMapping("/board/post")
+    @PostMapping()
     public void createPost(@RequestBody final PostRequest postRequest) {
         postService.createPost(postRequest.title());
     }
 
-    @GetMapping("/board")
+    @GetMapping()
     public BaseResponse<List<Post>> getAllPosts() {
         return BaseResponse.success(postService.getAllPost());
     }
 
-    @GetMapping("/board/{post-id}")
+    @GetMapping("/{post-id}")
     public BaseResponse<Post> getPostById(@PathVariable("post-id") Long postId) {
         return BaseResponse.success(postService.getPostById(postId));
     }
 
-    @DeleteMapping("/board/{post-id}/delete")
+    @DeleteMapping("/{post-id}")
     public BaseResponse<Boolean> deletePostById(@PathVariable("post-id") Long postId) {
         return BaseResponse.success(postService.deletePostById(postId));
     }
 
-    @PatchMapping("/board/{post-id}/modify")
+    @PatchMapping("/{post-id}")
     public BaseResponse<Boolean> updatePostTitle(@PathVariable("post-id") Long postId, @RequestBody final PostRequest postRequest) {
         return BaseResponse.success(postService.updatePostTitle(postId, postRequest.title()));
     }
