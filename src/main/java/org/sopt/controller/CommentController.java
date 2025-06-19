81da213b5d7c2eeabe11c1dee2c86de5b382247a
JIEUNI <jieundaeun@naver.com>
2025-06-19 14:40:20 +0900

    [chore] #9 엔드포인트 명확하게 수정

--- src/main/java/org/sopt/controller/CommentController.java
+++ src/main/java/org/sopt/controller/CommentController.java
@@ -10,35 +10,37 @@ import org.sopt.service.CommentService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/comment")
+@RequestMapping(value = "/board/{postId}/comments") // "board/{postId}에 속한 comments" 라고 명시
 public class CommentController {
     private final CommentService commentService;
 
     protected CommentController(CommentService commentService) { this.commentService = commentService; }
 
-    @PostMapping("/{post-id}")
+    @PostMapping()
     public BaseResponse<?> createComment(
             @RequestHeader Long userId,
-            @PathVariable("post-id") Long postId,
+            @PathVariable("postId") Long postId,
             @Valid @RequestBody final CommentCreateRequest commentCreateRequest
     ) {
         commentService.createComment(userId, postId, commentCreateRequest);
         return BaseResponse.success(SuccessMessage.CREATED);
     }
 
-    @PatchMapping("/{comment-id}")
+    @PatchMapping("/{commentId}")
     public BaseResponse<CommentResponse> updateComment(
             @RequestHeader Long userId,
-            @PathVariable("comment-id") Long commentId,
+            @PathVariable("postId") Long postId,
+            @PathVariable("commentId") Long commentId,
             @RequestBody final CommentUpdateRequest commentUpdateRequest
     ) {
         return BaseResponse.success(SuccessMessage.OK, commentService.updateComment(commentId, userId, commentUpdateRequest));
     }
 
-    @DeleteMapping("/{comment-id}")
+    @DeleteMapping("/{commentId}")
     public BaseResponse<?> deleteCommentById(
             @RequestHeader Long userId,
-            @PathVariable("comment-id") Long commentId
+            @PathVariable("postId") Long postId,
+            @PathVariable("commentId") Long commentId
     ) {
         commentService.deleteComment(userId, commentId);
         return BaseResponse.success(SuccessMessage.OK);
